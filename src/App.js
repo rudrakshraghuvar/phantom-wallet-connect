c0b6c90d80f7fa383a4d3678570da5866786bace
Rudra Pratap Singh <rpsingh2610@gmail.com>
2024-04-05 21:37:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+const { Web3 } = require('web3');
+const web3 = new Web3(window.phantom.ethereum);
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [balance, setBalance] = useState("");
+
+  const connectPhantom = async () => {
+    if (window.phantom.ethereum !== "undefined") {
+      // either we create instance via web3 1st method or we do directly by below method
+      // const accounts = await window.ethereum.request({ method: "eth_requestAccounts"});
+      await window.phantom.ethereum.enable();
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+      const bal = await web3.eth.getBalance(accounts[0]);
+      setBalance(web3.utils.fromWei(bal, "ether"));
+      window.ethereum.on('accountsChanged', async (newAccounts) => {
+        setAccount(newAccounts[0]);
+        const bal = await web3.eth.getBalance(newAccounts[0]);
+        setBalance(web3.utils.fromWei(bal, "ether"));
+      });
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={connectPhantom}>Connect To Phantom Wallet</button>
+      <p>Account Address:{account}</p>
+      <p>Balance:{balance}</p>
     </div>
   );
 }
